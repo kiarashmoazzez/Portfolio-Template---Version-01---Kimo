ecb256b414756c7eb4534f49c8b93eceb7a5fa10
kiarashmoazzez <kiarashmoazzez@gmail.com>
2026-04-24 16:16:58 +0200

    Add project config and app entry

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import Homepage from "../imports/Homepage/Homepage";
+import { MotionLayer } from "./components/motion-layer";
+
+export default function App() {
+  return (
+    <MotionLayer>
+      <Homepage />
+    </MotionLayer>
+  );
+}
